d09b203896d0a1a1d665dd841df8bd557f8389d9
nizar-sys <raspbery132@gmail.com>
2022-07-16 22:24:09 +0700

    edit class menu

--- resources/views/_partials/menus_frontend.blade.php
+++ resources/views/_partials/menus_frontend.blade.php
@@ -4,10 +4,10 @@ $userRole = Auth::user()?->role;
 @endphp
 
 <li><a class="{{ $routeActive == 'landing.home' ? 'active' : '' }}" href="{{ route('landing.home') }}" href="/">Home</a></li>
-<li><a clas="{{ $routeActive == 'landing.shop' ? 'active' : '' }}" href="{{ route('landing.shop') }}">Shop</a></li>
-<li><a clas="{{ $routeActive == 'landing.home' ? 'active' : '' }}" href="{{ route('landing.home') }}">Blog</a></li>
-<li><a clas="{{ $routeActive == 'landing.home' ? 'active' : '' }}" href="{{ route('landing.home') }}">About</a></li>
-<li><a clas="{{ $routeActive == 'landing.home' ? 'active' : '' }}" href="{{ route('landing.home') }}">Contact</a></li>
+<li><a class="{{ $routeActive == 'landing.shop' ? 'active' : '' }}" href="{{ route('landing.shop') }}">Shop</a></li>
+<li><a class="{{ $routeActive == 'landing.home' ? 'active' : '' }}" href="{{ route('landing.home') }}">Blog</a></li>
+<li><a class="{{ $routeActive == 'landing.home' ? 'active' : '' }}" href="{{ route('landing.home') }}">About</a></li>
+<li><a class="{{ $routeActive == 'landing.home' ? 'active' : '' }}" href="{{ route('landing.home') }}">Contact</a></li>
 <li id="lg-bag"><a clas="{{ $routeActive == 'landing.home' ? 'active' : '' }}" href="{{ route('landing.home') }}"><i
             class="fa fa-shopping-bag"></i></a></li>
 <a href="#" id="close"><i class="fa fa-times"></i></a>
